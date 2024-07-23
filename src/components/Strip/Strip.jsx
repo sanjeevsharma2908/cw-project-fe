909daeb889c04a91056516f8a265843aade36132
sanjeev sharma <ssharma2418@outlook.com>
2024-07-23 17:15:47 +0530

    offer image and some some responsiveness was added to the home page

--- src/components/Strip/Strip.jsx
+++ src/components/Strip/Strip.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import './strip.css'
+import React from "react";
+import "./strip.css";
 
 const Strip = () => {
   return (
-   <>
-   <div className="welcome-message">
-     <h1>Welcome to Butterfly</h1>
-   </div>
-   </>
-  )
-}
+    <>
+      <div className="welcome-message">
+        <h1>Welcome to Butterfly</h1>
+      </div>
+      <div className="offer-image">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0248/3473/6191/files/7_BANNER_1200X200_1.jpg?v=1715036262"
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
 
-export default Strip
+export default Strip;
